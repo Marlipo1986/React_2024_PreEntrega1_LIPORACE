5778db392672c8147a7609204e518745910b7e29
Martin Liporace <marlipo1986@gmail.com>
2024-03-17 12:15:04 -0300

    Ultimos detalles previos a la segunda preentrega

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,43 @@
-import NavBar from "./Components/Navbar/NavBar";
-import ItemListContainer from "./Components/ItemListContainer/ItemListContainer";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar/NavBar";
+//import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
+//import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
+
+//PAGES
+//import Bebidas from "./Pages/Bebidas";
+//import Entradas from "./Pages/Entradas";
+//import PlatosPrincipales from "./Pages/PlatosPrincipales";
+//import Postres from "./Pages/Postres";
+//import Guarniciones from "./Pages/Guarniciones";
+import MenuCompleto from "./Pages/MenuCompleto";
+//import Comanda from "./Pages/Comanda";
+import NotFound from "./Pages/NotFound";
+import DetallePlato from "./Pages/DetallePlato";
 
 const App = () => {
   return (
-    <div className="App">
-      <header>
-        <NavBar />
-      </header>
-      <div className="saludo">
-        <ItemListContainer greeting={"Bienvenido, qué comes hoy?"} />
-      </div>
+    <Router>
+      <div className="App">
+          <Navbar />
+        <Routes className="saludo">
+          <Route path="/" element= {<MenuCompleto />} />
+          <Route path="/MenuCompleto" element={<MenuCompleto/>} />
+          <Route path="/Detalle/:id" element={<DetallePlato/>} />
+          {/* <Route path="/Entradas" element={<Entradas/>} />
+          <Route path="/PlatosPrincipales" element={<PlatosPrincipales/>} />
+          <Route path="/Guarniciones" element={<Guarniciones/>} />
+          <Route path="/Postres" element={<Postres/>} />
+          <Route path="/Bebidas" element={<Bebidas/>} />
+          <Route path="/Comanda" element={<Comanda/>} /> */}
+          <Route path="*" element={<NotFound/>} />
+        </Routes>
+      
     </div>
+    </Router>
   );
 };
 
 export default App;
+
+
+{/* <ItemListContainer greeting='Hola, bienvenidos! Que comemos hoy?'/> */}
